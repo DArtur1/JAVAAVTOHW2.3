1d9c30e78d4cc41fd88c9b1840348cfa7f9979a2
DudkaAV_QA <arturdudka@yandex.ru>
2023-06-18 04:32:55 +0700

    again positive

--- src/test/java/PostmanEchoTest.java
+++ src/test/java/PostmanEchoTest.java
@@ -11,14 +11,14 @@ public class PostmanEchoTest {
         given()
                 .baseUri("https://postman-echo.com")
                 .contentType("text/plain; charset=UTF-8")
-                .body("что-то НЕ для постмана")// отправляемые данные (заголовки и query можно выставлять аналогично)
+                .body("фуууух")// отправляемые данные (заголовки и query можно выставлять аналогично)
 // Выполняемые действия
                 .when()
                 .post("/post")
 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("что-то для постмана"))
+                .body("data", equalTo("фуууух"))
         ;
     }
 }
